68aef77fd438eb1a9028ece8c12a4c8a90a134d6
Ale Candia <alejandrogcandia@gmail.com>
2025-10-16 11:35:38 -0300

    reload

--- SERVIDOR/Index.js
+++ SERVIDOR/Index.js
@@ -9,7 +9,5 @@ conn.sync().then(()=>{
     console.log('db sincronizada')
     server.listen(PORT,()=>{
         console.log('Servidor Listo en puerto : '+PORT)
-        
-    });
-    
+    }); 
 });
