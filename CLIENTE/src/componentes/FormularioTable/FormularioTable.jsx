1f1677ea594acc9a7c697479ccf4dfdc192f5367
Ale Candia <alejandrogcandia@gmail.com>
2025-10-24 12:03:57 -0300

    se crea componente para aditar y ver formulario

--- CLIENTE/src/componentes/FormularioTable/FormularioTable.jsx
+++ CLIENTE/src/componentes/FormularioTable/FormularioTable.jsx
@@ -45,7 +45,7 @@ const FormularioTable = () => {
             <tbody>
               {formularios.map((formulario) => (
                 <tr key={formulario.id}>
-                  <td>{formulario.email}</td>
+                  <td>{formulario.id}</td>
                   <td>{new Date(formulario.createdAt).toLocaleString()}</td>
                   <td>
                     {formulario.read ? (
--- CLIENTE/src/componentes/RenderForm/RenderForm.jsx
+++ CLIENTE/src/componentes/RenderForm/RenderForm.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
+import styles from './RenderForm.module.css'; // Importamos el archivo CSS Module
 
 const RenderForm = ({ formulario, onClose, onSave }) => {
-  // Usamos el estado local para gestionar los cambios en los inputs
-  const [isEditing, setIsEditing] = useState(false);  // Para saber si estamos en modo edición
+  const [isEditing, setIsEditing] = useState(false);  
   const [formData, setFormData] = useState({ ...formulario });
 
-  // Función para manejar el cambio en los inputs
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -14,68 +13,74 @@ const RenderForm = ({ formulario, onClose, onSave }) => {
     }));
   };
 
-  // Función para habilitar el modo edición
   const handleEdit = () => {
     setIsEditing(true);
   };
 
-  // Función para guardar los cambios
   const handleSave = () => {
-    onSave(formData);  // Llamamos al callback de guardar, pasando los datos actualizados
-    setIsEditing(false); // Deshabilitamos el modo edición
+    onSave(formData);  
+    setIsEditing(false); 
   };
 
   return (
-    <div className="formDetailsContainer">
-      <h3>Detalles del Formulario</h3>
-      <div className="formDetails">
-        <div>
+    <div className={styles.formDetailsContainer}>
+      <h3 className={styles.formTitle}>Detalles del Formulario</h3>
+      <div className={styles.formDetails}>
+        <div className={styles.formField}>
           <label>ID:</label>
           <input
             type="text"
-            name="id"
-            value={formData.id}
+            name="id_numerico"
+            value={formData.id_numerico}
             disabled
+            className={styles.formInput}
           />
         </div>
-        <div>
+        <div className={styles.formField}>
           <label>Email:</label>
           <input
             type="email"
-            name="email"
-            value={formData.email}
+            name="id"
+            value={formData.id}
             onChange={handleChange}
             disabled={!isEditing}
+            className={styles.formInput}
           />
         </div>
-        <div>
+        <div className={styles.formField}>
           <label>Fecha de Creación:</label>
           <input
             type="text"
             name="createdAt"
             value={new Date(formData.createdAt).toLocaleString()}
             disabled
+            className={styles.formInput}
           />
         </div>
-        <div>
-          <label>Leído:</label>
-          <input
-            type="checkbox"
-            name="read"
-            checked={formData.read}
-            onChange={(e) => handleChange({ target: { name: 'read', value: e.target.checked } })}
-            disabled={!isEditing}
-          />
-        </div>
+     <div className={styles.formField}>
+ 
+  <div className={styles.formCheckboxWrapper}> {/* Contenedor para checkbox y label */}
+    <input
+      type="checkbox"
+      name="read"
+      checked={formData.read}
+      onChange={(e) => handleChange({ target: { name: 'read', value: e.target.checked } })}
+      disabled={!isEditing}
+      className={styles.formCheckbox}
+    />
+    <label htmlFor="read" className={styles.checkboxLabel}>Leído</label> {/* Aquí puedes ajustar el texto si lo deseas */}
+  </div>
+</div>
+
       </div>
-      <div className="actions">
-        <button onClick={handleEdit} disabled={isEditing}>
+      <div className={styles.actions}>
+        <button onClick={handleEdit} disabled={isEditing} className={styles.buttonEdit}>
           Editar
         </button>
-        <button onClick={handleSave} disabled={!isEditing}>
+        <button onClick={handleSave} disabled={!isEditing} className={styles.buttonSave}>
           Guardar
         </button>
-        <button onClick={onClose}>
+        <button onClick={onClose} className={styles.buttonClose}>
           × Cerrar
         </button>
       </div>
